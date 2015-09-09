b0895a3dfdff1c3e9607be5a561caf9662b072a2
Sakari Ruoho <finnishandy@gmail.com>
2015-09-09 15:26:25 +0100

    fooo

--- src/3d.js
+++ src/3d.js
@@ -11,7 +11,26 @@ $(document).on("mousemove",function(e) {
 });
 
 $( window ).scroll(function(e) {
-    var ay = ($(window).innerHeight()/2- window.pageYOffset)/10;
-    card.attr("style", "transform: rotateX("+ay+"deg); -webkit-transform: rotateX("+ay+"deg); -moz-transform: rotateX("+ay+"deg)");
-    console.log(window.pageYOffset);
+    var cards = Array.prototype.slice.call($(".card"));
+    var ay = window.pageYOffset;
+    var innerHeight = window.innerHeight;
+    var fullHeight = document.documentElement.scrollHeight;
+
+    console.log("full: " + fullHeight);
+
+    cards.forEach(function(card, idx) {
+        if (idx === 1) {
+            var scrollTop = card.scrollTop;
+            console.log("scroll top: " + parseInt(document.body.scrollTop / fullHeight));
+        }
+        var cardMid = card.offsetTop + parseInt(card.offsetHeight / 2, 10);
+        var cardPos = cardMid / innerHeight;
+        //console.log("card" + idx + ": " + cardPos);
+        card.setAttribute("style", "transform: translateZ(-"+ay+"px); -webkit-transform: translateZ(-"+ay+"px); -moz-transform: translateZ(-"+ay+"px)");
+    });
+    //var ay = ($(window).innerHeight()/2- window.pageYOffset)/10;
+
+    //card.attr("style", "transform: rotateX("+ay+"deg); -webkit-transform: rotateX("+ay+"deg); -moz-transform: rotateX("+ay+"deg)");
+
+    console.log(ay);
 });
